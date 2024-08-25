0d6ba9bc40af9928c1be155e7269f798d0e581d2
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-25 20:59:58 +0530

    Create attributes.py

---
+++ numpy/attributes.py
@@ -0,0 +1,29 @@
+#Import the 'numpy' library for use with the rest of the code.
+import numpy as np
+
+#Create An Array using 'numpy'
+ary = np.array([1,2,3,4,5])
+
+#Print the Array
+print(ary)
+
+#Print the type of the Array
+print(type(ary))
+
+#Print the dimension of the Array
+print(ary.ndim)
+
+#Print the shape of the Array
+print(ary.shape)
+
+#Print the data type of the Array
+print(ary.dtype)
+
+#Print the size of the Array
+print(ary.size)
+
+#Print the itemsize of the Array
+print(ary.itemsize)
+
+#Print the total size of the Array
+print(ary.nbytes)
