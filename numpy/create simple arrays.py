f09391df9bd8545f6cc36ed158640728f384fb06
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-25 21:45:12 +0530

    Create create simple arrays.py

---
+++ numpy/create simple arrays.py
@@ -0,0 +1,37 @@
+#Import 'numpy'
+import numpy as np
+
+#Create An Array with the element 0.
+zero = np.zeros((5))
+#Print the Array
+print(zero)
+
+#Create An Array with the element 1.
+one = np.ones((7))
+#Print the Array
+print(one)
+
+#Create An Array with empty() function.
+empty = np.empty((2))
+#Print the Array
+print(empty)
+
+#Create An Array with range() function.
+rang = np.arange(4)
+#Print the Array
+print(rang)
+
+#Create An Array with linspace() function.
+linespace = np.linspace(0,10,num=5)
+#Print the Array
+print(linespace)
+
+#Create An Array of even number.
+even = np.arange(2,20,2)
+#Print the Array
+print(even)
+
+#Create An Array of odd number.
+odd = np.arange(1,20,2)
+#Print the Array
+print(odd)
