64fd701062b4c548d7cbebb3dd0a89f740d147af
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-25 21:48:46 +0530

    Create sorting.py

---
+++ numpy/sorting.py
@@ -0,0 +1,12 @@
+#Import 'numpy'
+import numpy as np
+
+#Create an array
+arr = np.array([6,7,8,3,1,9,2,4,5])
+#Print the array
+print(arr)
+
+#Sort the array
+sort_array = np.sort(arr)
+#Print the array
+print(sort_array)
