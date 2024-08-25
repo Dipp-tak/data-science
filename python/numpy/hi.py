19bffc10dd7c4f6e556272dd170ddc841375ddf4
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-25 22:36:30 +0530

    Update hi.py

--- python/numpy/hi.py
+++ python/numpy/hi.py
@@ -1,1 +1,14 @@
-print('hi')
+#Adding element in Array
+
+#Import 'numpy'
+import numpy as np
+
+#Create An Array
+arr = np.array([1,2,3,4,5,7,8,9])
+print(arr)
+
+# I want to add number 6 at the fifth index after 5 and before 7
+
+#new_arr = np.insert(old array name,index,element)
+new_arr = np.insert(arr,5,6)
+print(new_arr)
