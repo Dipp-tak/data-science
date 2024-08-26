4b038a83e666a7d9cd3f0454a7fc848ab26196a9
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-27 01:11:02 +0530

    Update reshape.py

--- python/numpy/reshape.py
+++ python/numpy/reshape.py
@@ -2,44 +2,49 @@
 import numpy as np
 
 # 1-D Array to 2-D Array
+print("\n1-D to 2-D Array Conversation \n")
 
 #Create An 1-D Array
 arr1 = np.array([1,2,3,4,5,6,7,8,9])
-print(arr1)
-print(arr1.shape)
-print(arr1.ndim)
+print("1-D Array: ",arr1)
+print("Shape of 1-D Array:",arr1.shape)
+print("The dimenson of 1-D Array:",arr1.ndim,"\n")
 
 #Reshape the array in (3,3)
 new_arr1 = arr1.reshape(3,3)
-print(new_arr1)
-print(new_arr1.shape)
-print(new_arr1.ndim)
+print("Convert Array's: \n",new_arr1)
+print("Convert Array's shape: ",new_arr1.shape)
+print("Convert Array's Dimenson: ",new_arr1.ndim) 
 
 #2-D Array to 3-D Array
+print("\n2-D to 3-D Array Conversation \n")
 
 #Create An 2-D Array
 arr2 = np.array([[1,2,3],[4,5,6]])
-print(arr2)
-print(arr2.shape)
-print(arr2.ndim)
+print("2-D Array: \n",arr2)
+print("Shape of 2-D Array:",arr2.shape)
+print("The dimenson of 2-D Array:",arr2.ndim,"\n")
+
 
 #Reshape the array in (1,2,3)
 new_arr2 = arr2.reshape(1,2,3)
-print(new_arr2)
-print(new_arr2.shape)
-print(new_arr2.ndim)
+print("Convert Array's: \n",new_arr2)
+print("Convert Array's shape: ",new_arr2.shape)
+print("Convert Array's Dimenson: ",new_arr2.ndim) 
+
 
 #3-D Array to 2-D Array
+print("\n3-D to 1-D Array Conversation \n")
 
 #Create An 3-D Array
 arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
-print(arr3)
-print(arr3.shape)
-print(arr3.ndim)
+print("3-D Array: \n",arr3)
+print("Shape of 3-D Array:",arr3.shape)
+print("The dimenson of 3-D Array:",arr3.ndim,"\n")
 
 #Reshape the array in (12,)
 new_arr3 = arr3.reshape(12)
-print(new_arr3)
-print(new_arr3.shape)
-print(new_arr3.ndim)
+print("Convert Array's: \n",new_arr3)
+print("Convert Array's shape: ",new_arr3.shape)
+print("Convert Array's Dimenson: ",new_arr3.ndim)
 
