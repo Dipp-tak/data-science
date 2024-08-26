2c264afe7f5ddc60ebcaba5e3925190fcbf84c12
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-27 00:57:16 +0530

    Update indexing and slicing.py

--- python/numpy/indexing and slicing.py
+++ python/numpy/indexing and slicing.py
@@ -3,45 +3,45 @@ import numpy as np
 
 #Create a Array and print it.
 arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-print(arr)
+print("The Array is : ",arr)
 
 #Access the element of array using index.
 #Array's Name[Index].
 #In Array indexing start with 0.
-print(arr[0]) 
+print("Print the 1st element of Array : ",arr[0]) 
 
 #Print Index the 8.
-print(arr[8])
+print("Print the elemnet whose index is 8: ",arr[8])
 
-#Print with negative index.
-print(arr[-5])
+#Print with using negative index.
+print("Print the element whose index is -5: ",arr[-5])
 
 #Slicing the Array
 
 #Print Element less than 10.
 less_10 = arr[arr<10]
-print(less_10)
+print("Print elements less than 10",less_10)
 
 #Print Element greater than 10.
 greater_10 = (arr>10)
-print(arr[greater_10])
+print("print elements greater than 10",arr[greater_10])
 
 #let's print greater than 5 and less than 11.
 bwt = ((arr>5) & (arr<11))
-print(arr[bwt])
+print("print the elements greater than 5 and below than10:",arr[bwt])
 
 #Let's Print element divisible by 3.
 div = (arr % 3 == 0)
-print(arr[div])
+print("Print the elements divided by 3 :",arr[div])
 
 #Let's Print element with index slicingsling.
 slic_data = arr[0:5]
-print(slic_data)
+print("The elements upto index 4 is:",slic_data)
 
 #Print the element between index 5 to 11.
 slic_data2 = arr[5:11]
-print(slic_data2)
+print("The elements between index 5 and 11:",slic_data2)
 
-#Print the element 9,8,7,6 & 5 with using negative index.
+#Print the element 5,6,7,8 and 9 with using negative index.
 neg_slic_data = arr[-11:-6]
-print(neg_slic_data)
+print("Print 5,6,7,8 and 9 with uing negative index",neg_slic_data)
