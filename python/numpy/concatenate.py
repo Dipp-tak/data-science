81fee00632f6d1d4a2458be7b6eb28b94eaa2e0a
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-26 23:32:41 +0530

    Update concatenate.py

--- python/numpy/concatenate.py
+++ python/numpy/concatenate.py
@@ -6,10 +6,10 @@ arr1 = np.array([5,6,7,8,9])
 arr2 = np.array([1,2,3,4])
 
 #Print the creating Arrys
-print(arr1)
-print(arr2)
+print("The 1st Array is : ",arr1)
+print("The 2nd Array is : ",arr2)
 
 #Concatenate arr1 & arr2
 arr  = np.concatenate((arr1,arr2))
 #Print the final Array
-print(arr)
+print("After the Concatenation of 1st and 2nd Array, the final Array is : ",arr)
