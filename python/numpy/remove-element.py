5b4213a8086e25f88889255c3fede4bc8f2f6dc9
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-26 23:15:12 +0530

    Update remove-element.py

--- python/numpy/remove-element.py
+++ python/numpy/remove-element.py
@@ -5,8 +5,8 @@ import numpy as np
 
 #Create An Array
 arr = np.array([1,2,3,4,5,6,7,8,9])
-print(arr)
+print("The Array is: ",arr)
 
 # I want to remove element 2,4 & 8 from the Array, the index of 2,4,8 is 1,3,7 so removing the element using the their index
 new_arr = np.delete(arr,[1,3,7])
-print(new_arr)
+print("Ater deleting element the new Array is : ",new_arr)
