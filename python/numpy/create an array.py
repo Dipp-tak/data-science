bb8dfdeae1c981e23de1d67ac2f3d101c6ef5526
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-27 00:37:39 +0530

    Update create an array.py

--- python/numpy/create an array.py
+++ python/numpy/create an array.py
@@ -1,12 +1,13 @@
 import numpy as np
 #1-D Array
 a = np.array([1,2,3])
-a
+print("The one dimension Array is : ",a)
 
 #2-D Array
 b = np.array([[1,2,3],[4,5,6]])
-b
+print("The two dimension Array is : ",b)
 
 #3-D Array
 c = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
-c
+print("The three dimension Array is : \n",c)
+
