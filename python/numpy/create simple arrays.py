bb11687809e6b2520ed3e72abe71cfbb539709e6
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-27 00:45:11 +0530

    Update create simple arrays.py

--- python/numpy/create simple arrays.py
+++ python/numpy/create simple arrays.py
@@ -2,36 +2,36 @@
 import numpy as np
 
 #Create An Array with the element 0.
-zero = np.zeros((5))
+zero_arr = np.zeros((5))
 #Print the Array
-print(zero)
+print("The Array With Element Zero : \n",zero_arr)
 
 #Create An Array with the element 1.
-one = np.ones((7))
+one_arr = np.ones((7))
 #Print the Array
-print(one)
+print("The Array With Element one : \n",one_arr)
 
 #Create An Array with empty() function.
-empty = np.empty((2))
+empty_arr = np.empty((2))
 #Print the Array
-print(empty)
+print("The Array With empty() function : \n",empty_arr)
 
 #Create An Array with range() function.
-rang = np.arange(4)
+rang_arr = np.arange(4)
 #Print the Array
-print(rang)
+print("The Array With range() function : \n",rang_arr)
 
 #Create An Array with linspace() function.
-linespace = np.linspace(0,10,num=5)
+linespace_arr = np.linspace(0,10,num=5)
 #Print the Array
-print(linespace)
+print("The Array With linspace() function : \n",linespace_arr)
 
 #Create An Array of even number.
-even = np.arange(2,20,2)
+even_arr = np.arange(2,20,2)
 #Print the Array
-print(even)
+print("Even numbers Arry 0 to 20 : \n",even_arr)
 
 #Create An Array of odd number.
-odd = np.arange(1,20,2)
+odd_arr = np.arange(1,20,2)
 #Print the Array
-print(odd)
+print("Odd Numbers Array 0 to 20 : \n",odd_arr)
