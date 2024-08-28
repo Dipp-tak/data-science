fde9ca2517939ecd0a8d2c8acf06b1003440baac
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-28 22:48:37 +0530

    Create Create dataframe from dictionary.py

---
+++ python-library-for-practice/pandas/DataFrame/Create dataframe from dictionary.py
@@ -0,0 +1,5 @@
+#Create DataFrame from dictionary
+my_dict = {'a':'Apple','b':'Ball','c':'Cat','d':'Dog','e':'Egg'}
+my_DataFrame = pd.DataFrame(my_dict,index=[1,2,3,4,5])
+#Print the Data-Frame
+my_DataFrame
