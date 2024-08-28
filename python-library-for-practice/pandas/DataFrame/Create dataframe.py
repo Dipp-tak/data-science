ec97b9e6cb51f8f009f473452a6ce47d5082ac34
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-28 22:45:21 +0530

    Create Create dataframe.py

---
+++ python-library-for-practice/pandas/DataFrame/Create dataframe.py
@@ -0,0 +1,12 @@
+#Where we see The series Data Structure and some operation on it.
+#Import pandas for use in codes.
+import pandas as pd
+
+#Create a list and print it.
+my_list = [1,2,3,4,5]
+print("The list is : ",my_list)
+
+#Create a Data-Frame from the list and print it.
+my_DataFrame = pd.DataFrame(my_list)
+#Print the Data-Frame
+my_DataFrame
