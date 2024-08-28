9180362add31c3052151e1d7f71c2a47a4358dad
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-29 01:09:12 +0530

    Create addition-bwt-two-columns.py

---
+++ python-library-for-practice/pandas/Arithmetic Operation/DataFrame/addition-bwt-two-columns.py
@@ -0,0 +1,9 @@
+#Import Pandas 
+import pandas as pd
+#Create A data Frame
+my_df = pd.DataFrame({'Numbers':[1,2,3,4,5],'Numbers_2':[6,7,8,9,10]})
+#Addition operation between two columns
+my_df['Result_Numbers'] = my_df['Numbers'] + my_df['Numbers_2']
+# Print the Data Frame where we have the addition value in another
+# column[Result_Numbers]
+my_df
