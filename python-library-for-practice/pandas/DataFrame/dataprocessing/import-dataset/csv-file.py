791801faeb95ab598d7aa2a576123ab05639211b
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-30 22:06:01 +0530

    Create csv-file.py

---
+++ python-library-for-practice/pandas/DataFrame/dataprocessing/import-dataset/csv-file.py
@@ -0,0 +1,10 @@
+#Import Dataset from files
+
+#Import pandas
+import pandas as pd
+
+#Import dataset
+df = pd.read_csv('/content/practice-dataset.csv')
+df
+
+#I am done this code in google colab. So At frist upload data set and then copy the path and put the path inside read.csv()
